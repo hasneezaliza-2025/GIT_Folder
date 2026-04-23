12e676f6f6bd8f767f0233141d919d7a1d9000e1
Hasneezaliza <hasneezaliza@yahoo.com>
2026-04-23 13:20:24 +0800

    My second modification

--- simple_calculator.py
+++ simple_calculator.py
@@ -23,10 +23,10 @@ def main():
         num2 = float(input("Enter second number 2: "))
         
         print("\nSelect operation:")
-        print("1. Add +")
-        print("2. Subtract -")
-        print("3. Multiply *")
-        print("4. Divide /")
+        print("1. Add (+)")
+        print("2. Subtract (-)")
+        print("3. Multiply (*)")
+        print("4. Divide (/)")
         
         choice = input("Enter choice (1/2/3/4): ")
         
